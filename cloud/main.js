c2f66f4f448ed15c324d546790709c51d4b7d93f
crazyphage <banana@eurus.cn>
2014-09-29 09:35:48 +0800

    testobj save

--- cloud/main.js
+++ cloud/main.js
@@ -4,4 +4,11 @@ require("cloud/app.js");
 
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
+  var TestObject = AV.Object.extend("TestObject");
+  var testObject = new TestObject();
+  testObject.save({foo: "bar"}, {
+    success: function(object) {
+      alert("AVOS Cloud works!");
+    }
+  });
 });
