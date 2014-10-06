e99c93ba6571fdcfd5b751ea0b8c3c2167fc6854
Crazy_Phage <ufozhengli@163.com>
2014-10-06 14:54:25 +0800

    不知道有没有bug

--- public/js/dash.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	AV.$ = jQuery;
-	AV.initialize( "eenezb2s4tnlbytmv8rt3ndrv4qiux13jg7s90n7ff72kvoa",
-		"flo4ra0v81t51v0ug33pzwvm4xsclmgqo23fqht4iendgoio");
-	
-	$('.elastic-textarea').elastic();
-
-	
-});
