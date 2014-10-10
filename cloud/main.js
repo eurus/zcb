c10bde71a37dc4999f054412738c3f59d02dfcae
Crazy_Phage <ufozhengli@163.com>
2014-10-10 15:17:33 +0800

    cloud func

--- cloud/main.js
+++ cloud/main.js
@@ -10,8 +10,7 @@ function forceLogin(res){
 
 AV.Cloud.define('findUserById', function(req, res){
   forceLogin(res);
-
-  var id = req.params.id || '';
+  var id = req.params.uid || '';
   userCon.findUserById(id).then(function(user){
     res.success(user);
   }, function(){
@@ -21,7 +20,6 @@ AV.Cloud.define('findUserById', function(req, res){
 
 AV.Cloud.define('findUserByName', function(req, res){
   forceLogin(res);
-
   var name = req.params.username || '';
   userCon.findUserByName(name).then(function(user){
     res.success(user);
@@ -30,18 +28,55 @@ AV.Cloud.define('findUserByName', function(req, res){
   });
 });
 
-AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-  var TestObject = AV.Object.extend("TestObject");
-  var testObject = new TestObject();
-  testObject.save({foo: "bar"}, {
-    success: function(object) {
-      console.log("AVOS Cloud works!");
+AV.Cloud.define("getOrderList", function(req, res) {
+  var Order = AV.Object.extend("Order");
+  var User = AV.Object.extend("_User");
+  var user = new User();
+  user.id = req.params.uid
+
+  var query = new AV.Query(Order);
+  query.include("user");
+  query.include("package");
+  query.include("car");
+  query.include("store");
+  query.include("salesman");
+  query.include("address");
+  query.include("operator");
+
+  query.equalTo("user",user);
+  query.find({
+    success: function(results) {
+      console.log("Successfully retrieved " + results.length + " scores.");
+      res.success(results);
+    },
+    error: function(error) {
+      console.log("Error: " + error.code + " " + error.message);
     }
   });
-  AV.Cloud.requestSmsCode('18651206017').then(function(){
-    console.log("sms works!");
-  }, function(err){
-    console.log("sms not works!");
-  });
+});
+
+AV.Cloud.define("getCustomerService", function(req, res) {
+  console.log(req.params.temp);
+  temp = req.params.temp
+  console.log(typeof(temp));
+  // cspid = 
+  if(temp == "n") {
+  //return customer servece peerid only 
+    result = {
+      cspid: "lala"
+    }
+    res.success(result);
+  } else {
+  //return customer servece peerid and customer peerid
+    n = (new Date()).getTime();
+    str1 = n + "";
+    str2 = Math.random().toString(36).substring(7);
+
+    result = {
+      cspid: "lala",
+      cpid: str1 + str2
+    }
+    res.success(result);
+  }
+
 });
