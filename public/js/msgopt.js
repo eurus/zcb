b08234239fcab5b767cb84af58bae04cffe8e106
banana <ufozhengli@163.com>
2014-10-23 11:06:18 +0800

    新建msgopt.js文件分離單獨的聊天消息

---
+++ public/js/msgopt.js
@@ -0,0 +1,149 @@
+//append msg for init msg
+function appendSendMsg(peerId, content,timestamp){
+    // 这里是发送的消息append
+    str = _.template($('#text-tpl').html())({
+        peerId:peerId,
+        content:content,
+        avatar: localStorage[myPeerId+'-avatar'],
+        timestamp:timestamp,
+        klass:'from-me'
+    });
+    var elem = $(str);
+    $('#'+peerId).append(elem);
+    scrollToEnd(peerId);
+}
+
+function appendRecvMsg(data){
+    console.log('HOLY CRAP');
+    receive_msg = JSON.parse(data.msg);
+
+    var from_avatar = data.fromPeerId + "-avatar";
+    switch(receive_msg.InfoType) {
+        case "IMAGE":
+        str2 = _.template($('#image-tpl').html())({
+            peerId: data.fromPeerId,
+            content: receive_msg.Content,
+            avatar: localStorage[from_avatar] == 'undefined' ? 'img/user1.png' : localStorage[from_avatar],
+            timestamp: data.timestamp,
+            klass:'to-me'
+        });
+
+        break;
+
+        case "VOICE":
+
+        str2 = _.template($('#voice-tpl').html())({
+            peerId: data.fromPeerId,
+            content: receive_msg.Content,
+            avatar: localStorage[from_avatar] == 'undefined' ? 'img/user1.png' : localStorage[from_avatar],
+            timestamp: data.timestamp,
+            klass:'to-me'
+        });
+        break;
+
+        case "TEXT":
+
+        str2 = _.template($('#text-tpl').html())({
+            peerId: data.fromPeerId,
+            content: receive_msg.Content,
+            avatar: localStorage[from_avatar] == 'undefined' ? 'img/user1.png' : localStorage[from_avatar],
+            timestamp: data.timestamp,
+            klass:'to-me'
+        });                   
+        break;
+    }             
+    if (data.fromPeerId!=toPeerId){
+        var count = $('#user-'+data.fromPeerId+' .badge').html();
+        console.log('count = '+count);
+        count = parseInt(count) || 0;
+        count += 1;
+        console.log('count after = '+count);
+        $('#user-'+data.fromPeerId+' .badge').html(count); 
+    }
+    console.log("str2"+str2);
+    $('#'+data.fromPeerId).append(str2);
+
+    $('.msg.msg-voice > .audiojs').each(function(e){
+        $(this).replaceWith($('audio', $(this)));
+
+    })
+    audiojs.events.ready(function() {
+        var as = audiojs.createAll();
+    });
+
+    scrollToEnd(data.fromPeerId);
+
+}
+//prepend msg for load chat history
+
+function prependSendMsg(peerId, content,timestamp){
+    // 这里是发送的消息append
+    str = _.template($('#text-tpl').html())({
+        peerId:peerId,
+        content:content,
+        avatar: localStorage[myPeerId+'-avatar'],
+        timestamp:timestamp,
+        klass:'from-me'
+    });
+    var elem = $(str);
+    $('#'+peerId).append(elem);
+}
+
+function prependRecvMsg(data){
+    console.log('HOLY CRAP');
+    receive_msg = JSON.parse(data.msg);
+
+    var from_avatar = data.fromPeerId + "-avatar";
+    switch(receive_msg.InfoType) {
+        case "IMAGE":
+        str2 = _.template($('#image-tpl').html())({
+            peerId: data.fromPeerId,
+            content: receive_msg.Content,
+            avatar: localStorage[from_avatar] == 'undefined' ? 'img/user1.png' : localStorage[from_avatar],
+            timestamp: data.timestamp,
+            klass:'to-me'
+        });
+
+        break;
+
+        case "VOICE":
+
+        str2 = _.template($('#voice-tpl').html())({
+            peerId: data.fromPeerId,
+            content: receive_msg.Content,
+            avatar: localStorage[from_avatar] == 'undefined' ? 'img/user1.png' : localStorage[from_avatar],
+            timestamp: data.timestamp,
+            klass:'to-me'
+        });
+        break;
+
+        case "TEXT":
+
+        str2 = _.template($('#text-tpl').html())({
+            peerId: data.fromPeerId,
+            content: receive_msg.Content,
+            avatar: localStorage[from_avatar] == 'undefined' ? 'img/user1.png' : localStorage[from_avatar],
+            timestamp: data.timestamp,
+            klass:'to-me'
+        });                   
+        break;
+    }             
+    if (data.fromPeerId!=toPeerId){
+        var count = $('#user-'+data.fromPeerId+' .badge').html();
+        console.log('count = '+count);
+        count = parseInt(count) || 0;
+        count += 1;
+        console.log('count after = '+count);
+        $('#user-'+data.fromPeerId+' .badge').html(count); 
+    }
+    console.log("str2"+str2);
+    $('#'+data.fromPeerId).append(str2);
+
+    $('.msg.msg-voice > .audiojs').each(function(e){
+        $(this).replaceWith($('audio', $(this)));
+
+    })
+    audiojs.events.ready(function() {
+        var as = audiojs.createAll();
+    });
+}
